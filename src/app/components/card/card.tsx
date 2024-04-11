6c459ba8667c25667a4a9ea99d100669124c2ff2
Drsultanali <sultan_ali338@yahoo.com>
2024-04-12 00:50:45 +0500

    updated position and text

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -2,15 +2,15 @@ export default function Card() {
     return (
 
         <div className=" text-center flex min-h-full flex-col justify-center">
-            <div className=" max-w-[400px] px-3  items-center rounded-xl  border-white border-2 bg-white font-light ">
+            <div className=" max-w-[400px] px-3  items-center rounded-xl shadow-xl border-white border-2 bg-white font-light ">
                 <div className="mt-3 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" action="#" method="POST">
                         <div>
                             <div className="">
-                                <input id="email" name="email" type="email" placeholder=" Email address or phone number"
-                                    required className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset
-                                     ring-gray-300 placeholder:p-2 placeholder:text-gray-400 placeholder:text-lg focus:ring-2 focus:ring-inset
-                                      focus:ring-blue-400 sm:text-sm sm:leading-6" />
+                                <input id="email" name="email" type="email" placeholder="Email address or phone number"
+                                    required className=" w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset
+                                     ring-gray-300  placeholder:text-gray-400 pl-4 placeholder:text-lg text-lg focus:ring-2 focus:ring-inset
+                                      focus:ring-blue-400 inline-block " />
                             </div>
                         </div>
 
@@ -19,26 +19,28 @@ export default function Card() {
 
                             </div>
                             <div className="">
-                                <input id=" password" name=" password" type=" password" placeholder=" Password"
+                                <input id="password" name="password" type="password" placeholder="Password"
                                     required className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset
-                                ring-gray-300 placeholder:text-gray-400 placeholder:p-2 placeholder:text-lg focus:ring-2 focus:ring-inset
-                                 focus:ring-blue-400 sm:text-sm sm:leading-6" />
+                                ring-gray-300 placeholder:text-gray-400 pl-4 placeholder:text-lg text-lg focus:ring-2 focus:ring-inset
+                                 focus:ring-blue-400 show-app"/>
+                               
+
+                                </div>
+
                             </div>
 
-                        </div>
-
-                        <div>
-                            <button type="submit" className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-3 text-lg font-semibold leading-6 text-white shadow-sm
-                        hover:bg-[#0866FF] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+                            <div>
+                                <button type="submit" className="flex w-full justify-center rounded-md bg-[#0866FF] px-3 py-3 text-xl font-semibold leading-6 text-white shadow-sm
+                        hover:bg-[#1776F3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
                         focus-visible:outline-[#0866FF]">Log in</button>
-                        </div>
-                        <div className="text-sm mt-2">
-                            <a href="#" className=" text-center font-light text-[#0866FF] hover:text-blue-600 hover:underline tracking-wide">Forgotten password?</a>
-                        </div>
-                        <div>
-                            <button type="submit" className=" mb-5 max-w-40 justify-center rounded-md 
-                         bg-[#36A420] hover:bg-[#368a25] px-3 py-3 text-sm font-semibold text-white">Create new account</button>
-                        </div>
+                            </div>
+                            <div className="text-sm mt-2">
+                                <a href="#" className=" text-center font-light text-[#0866FF] hover:text-blue-600 hover:underline tracking-wide">Forgotten password?</a>
+                            </div>
+                            <div>
+                                <button type="submit" className=" mb-5 justify-center rounded-md 
+                         bg-[#42B72A] hover:bg-[#36A420] px-8 py-3 text-base font-semibold text-white">Create new account</button>
+                            </div>
                     </form>
 
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,24 @@ export default function Home() {
   return (
 
 
-    <main>
+    <div className="bg-[#F0F2F5] h-screen">
 
       <title>Facebook</title>
 
-      <body className=" bg-[#F0F2F5] ">
-        <div className="max-w-full h-auto flex flex-wrap lg:flex-row flex-col place-items-center lg:space-x-10 justify-center">
-          <div className="mt-5 flex justify-center flex-col  items-center lg:items-start">
+      <div className=" font-Helvatica">
+        <div className="max-w-full h-screen flex flex-wrap lg:flex-row flex-col place-items-center lg:space-x-10 justify-center">
+          <div className=" flex lg:mb-[7%] justify-center flex-col  items-center lg:items-start">
             <Image className="lg:ml-0" src="/facebookLogo.jpg" alt="logo" width={256} height={200} />
-            <div className=" text-center lg:text-start ">
+            <div className="text-center lg:text-start ">
               <h2 className=" text-black  lg:ml-0 xl:text-3xl text-[24px] w-[400px] xl:w-[550px]  ">Facebook helps you connect and share with people in your life.</h2>
             </div>
           </div>
-          <div className=" lg:mt-20 mt-10 w-[400px] md:w-[400px] ">
+          <div className="mt-5 w-[400px] :w-[400px] ">
             <Card />
           </div>
         </div>
-      </body>
-    </main>
+      </div>
+    </div>
   )
 
 }
